22f6042e75ccf4252a699187dd671b3eb351eb13
Andreas Fruth <andreas.fruth@gmail.com>
2017-07-13 07:51:36 +0300

    First commit

---
+++ server/main.js
@@ -0,0 +1,33 @@
+import { Meteor } from 'meteor/meteor';
+import { JsonRoutes } from 'fine-rest';
+
+Meteor.startup(() => {
+  JsonRoutes.setResponseHeaders({
+    "Cache-Control": "no-store",
+    "Pragma": "no-cache",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+  });
+
+  // code to run on server at startup
+  JsonRoutes.Middleware.use(function(req, res, next) {
+
+    if(req.query.error) {
+      JsonRoutes.sendResult(res, {
+        code: 401,
+        data: {
+          result: "ERROR"
+        }
+      });
+    } else next();
+  });
+
+  JsonRoutes.add('GET', '/test-route/', function(req, res, next) {
+    JsonRoutes.sendResult(res, {
+      data: {
+        result: "OK"
+      }
+    });
+  });
+});
